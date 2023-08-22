145d8ff0b9d0108a56017901bd00c192c758d03e
farling42 <github@amusingtime.uk>
2023-08-22 14:20:24 +0100

    Remove V9 support

--- scripts/export-markdown.js
+++ scripts/export-markdown.js
@@ -1,6 +1,6 @@
 import "./lib/jszip.min.js";
-import {TurndownService} from "./lib/turndown.js";
-import {turndownPluginGfm} from "./lib/turndown-plugin-gfm.js";
+import { TurndownService } from "./lib/turndown.js";
+import { turndownPluginGfm } from "./lib/turndown-plugin-gfm.js";
 
 const MODULE_NAME = "export-markdown";
 const FRONTMATTER = "---\n";
@@ -12,7 +12,7 @@ const FRONTMATTER = "---\n";
 
 function validFilename(name) {
     const regexp = /[<>:"/\\|?*]/g;
-    return name.replaceAll(regexp,'_');
+    return name.replaceAll(regexp, '_');
 }
 
 /**
@@ -20,19 +20,19 @@ function validFilename(name) {
  * @param {string} blob       The Blob of data
  * @param {string} filename   The filename of the resulting download
  */
-function saveDataToFile(blob, filename) {  
+function saveDataToFile(blob, filename) {
     // Create an element to trigger the download
     let a = document.createElement('a');
     a.href = window.URL.createObjectURL(blob);
     a.download = filename;
-  
+
     // Dispatch a click event to the element
-    a.dispatchEvent(new MouseEvent("click", {bubbles: true, cancelable: true, view: window}));
+    a.dispatchEvent(new MouseEvent("click", { bubbles: true, cancelable: true, view: window }));
     setTimeout(() => window.URL.revokeObjectURL(a.href), 100);
 }
 
 function folderpath(journal) {
-    let result="";
+    let result = "";
     let folder = journal.folder;
     while (folder) {
         const foldername = validFilename(folder.name);
@@ -65,115 +65,102 @@ function fileconvert(str, filename) {
 let turndownService, gfm;
 
 function convertHtml(doc, html) {
-	if (!turndownService) {
-		// Setup Turndown service to use GFM for tables
-		turndownService = new TurndownService({ headingStyle: "atx" });
+    if (!turndownService) {
+        // Setup Turndown service to use GFM for tables
+        turndownService = new TurndownService({ headingStyle: "atx" });
         gfm = turndownPluginGfm.gfm;
-		turndownService.use(gfm);
-	}
-	let markdown;
-	try {
-		markdown = turndownService.turndown(html);
+        turndownService.use(gfm);
+    }
+    let markdown;
+    try {
+        markdown = turndownService.turndown(html);
     } catch (error) {
-		console.warn(`Error: failed to decode html:`, html)
-	}
+        console.warn(`Error: failed to decode html:`, html)
+    }
 
-    function convertLink(str, id, section, label, offset, string, groups) {    
-        let linkeddoc = str.startsWith('@UUID') ? fromUuidSync(id, {relative:doc}) : game.journal.get(id);
+    function convertLink(str, id, section, label, offset, string, groups) {
+        let linkeddoc = str.startsWith('@UUID') ? fromUuidSync(id, { relative: doc }) : game.journal.get(id);
         if (!(linkeddoc instanceof JournalEntry || linkeddoc instanceof JournalEntryPage)) return str;
         let journal = linkeddoc.parent || linkeddoc;
-    
-        let filename = id.startsWith('.') ? validFilename(linkeddoc.name) : 
+
+        let filename = id.startsWith('.') ? validFilename(linkeddoc.name) :
             (journal.pages?.size > 1) ? (folderpath(journal) + "/" + validFilename(linkeddoc.name)) : folderpath(journal);
         if (label == filename)
             return `[[${filename}]]`;
         else
             return `[[${filename}|${label}]]`;
     }
-    
+
     // Convert all the links
     // Replace Journal Links
-	if (markdown.includes('@JournalEntry')) {
-		// The square brackets in @JournalEntry will already have been escaped! if text was converted to markdown
-		const pattern1 = /@([a-zA-Z]+)\\\[([^\]]*)\\\]{([^\}]*)}/g;
-		markdown = markdown.replaceAll(pattern1, convertLink);
-		// Foundry V10 allows markdown, which won't have escaped markers
-		const pattern2 = /@([a-zA-Z]+)\[([^\]]*)\]{([^\}]*)}/g;
-		markdown = markdown.replaceAll(pattern2, convertLink);
-	}
-	// Converted text has the first [ escaped
-	if (markdown.includes('@UUID\\[')) {
-		// The square brackets in @JournalEntry will already have been escaped!
+    if (markdown.includes('@JournalEntry')) {
+        // The square brackets in @JournalEntry will already have been escaped! if text was converted to markdown
+        const pattern1 = /@([a-zA-Z]+)\\\[([^\]]*)\\\]{([^\}]*)}/g;
+        markdown = markdown.replaceAll(pattern1, convertLink);
+        // Foundry V10 allows markdown, which won't have escaped markers
+        const pattern2 = /@([a-zA-Z]+)\[([^\]]*)\]{([^\}]*)}/g;
+        markdown = markdown.replaceAll(pattern2, convertLink);
+    }
+    // Converted text has the first [ escaped
+    if (markdown.includes('@UUID\\[')) {
+        // The square brackets in @JournalEntry will already have been escaped!
         const pattern = /@UUID\\\[([^#\\]+)(?:#([^\\]+))?\\\](?:{([^}]+)})?/g;
-		//cst pattern = /@UUID\\\[([a-zA-Z]*)\.([^\]]*)\\\]{([^\}]*)}/g;
-		markdown = markdown.replaceAll(pattern, convertLink);
-	}
-	// Converted markdown does NOT include the escape flag
-	if (markdown.includes('@UUID[')) {
+        //cst pattern = /@UUID\\\[([a-zA-Z]*)\.([^\]]*)\\\]{([^\}]*)}/g;
+        markdown = markdown.replaceAll(pattern, convertLink);
+    }
+    // Converted markdown does NOT include the escape flag
+    if (markdown.includes('@UUID[')) {
         const pattern = new RegExp(`@UUID\\[([^#\\]]+)(?:#([^\\]]+))?](?:{([^}]+)})?`, "g");
-		//const pattern = /@UUID\[([a-zA-Z]*)\.([^\]]*)\]{([^\}]*)}/g;
-	    markdown = markdown.replaceAll(pattern, convertLink);
-	}
-	// Replace file references
-	if (markdown.includes('![](')) {
-		//console.log(`File ${item.filename} has images`);
-		const filepattern = /!\[\]\(([^)]*)\)/g;
-		markdown = markdown.replaceAll(filepattern, fileconvert);
-	}
+        //const pattern = /@UUID\[([a-zA-Z]*)\.([^\]]*)\]{([^\}]*)}/g;
+        markdown = markdown.replaceAll(pattern, convertLink);
+    }
+    // Replace file references
+    if (markdown.includes('![](')) {
+        //console.log(`File ${item.filename} has images`);
+        const filepattern = /!\[\]\(([^)]*)\)/g;
+        markdown = markdown.replaceAll(filepattern, fileconvert);
+    }
 
-	return markdown;
+    return markdown;
 }
 
 function oneJournal(zip, journal) {
     let topfolder = validFilename(journal.name);
-    if (journal.pages) {
-        let onepage = journal.pages.size===1;
-        const dirname  = onepage ? folderpath(journal) : (folderpath(journal) + "/" + validFilename(journal.name));
-        if (!onepage) {
-            // TOC page 
-            // This is a Folder note, so goes INSIDE the folder for this journal entry
-            const notename = journal.name;
-            let markdown = "## Table of Contents\n";
-            for (let page of journal.pages) {
-                markdown += `\n- [[${validFilename(page.name)}]]`;
-            }
-            markdown = FRONTMATTER + `title: "${notename}"\n` + `aliases: "${notename}"\n` + `foundryId: ${journal.uuid}\n` + FRONTMATTER + markdown;
-            zip.file(`${dirname}/${validFilename(notename)}.md`, markdown, {binary:false});
-        }
-
-        for (const page of journal.pages) {
-            let markdown;
-            const notename = onepage ? journal.name : page.name;
-            switch (page.type) {
-                case "text":
-                    switch (page.text.format) {
-                        case 1: // HTML
-                            markdown = convertHtml(page, page.text.content);
-                            break;
-                        case 2: // MARKDOWN
-                            markdown = page.text.markdown;
-                            break;
-                    }
-                    break;
-                case "image": case "pdf": case "video":
-                    if (page.src) markdown = `![](${page.src})`;
-                    break;
-            }
-            if (markdown) {
-                markdown = FRONTMATTER + `title: "${page.name}"\n` + `aliases: "${journal.name}"\n` + `foundryId: ${page.uuid}\n` + FRONTMATTER + (markdown || "");
-                zip.file(`${dirname}/${validFilename(notename)}.md`, markdown, {binary:false});
-            }
-        }
-    } else {
-        // FOUNDRY v9 or earlier
+    let onepage = journal.pages.size === 1;
+    const dirname = onepage ? folderpath(journal) : (folderpath(journal) + "/" + validFilename(journal.name));
+    if (!onepage) {
+        // TOC page 
+        // This is a Folder note, so goes INSIDE the folder for this journal entry
         const notename = journal.name;
+        let markdown = "## Table of Contents\n";
+        for (let page of journal.pages) {
+            markdown += `\n- [[${validFilename(page.name)}]]`;
+        }
+        markdown = FRONTMATTER + `title: "${notename}"\n` + `aliases: "${notename}"\n` + `foundryId: ${journal.uuid}\n` + FRONTMATTER + markdown;
+        zip.file(`${dirname}/${validFilename(notename)}.md`, markdown, { binary: false });
+    }
+
+    for (const page of journal.pages) {
         let markdown;
-        if (journal.content) {
-            markdown = convertHtml(page, page.text.content);
+        const notename = onepage ? journal.name : page.name;
+        switch (page.type) {
+            case "text":
+                switch (page.text.format) {
+                    case 1: // HTML
+                        markdown = convertHtml(page, page.text.content);
+                        break;
+                    case 2: // MARKDOWN
+                        markdown = page.text.markdown;
+                        break;
+                }
+                break;
+            case "image": case "pdf": case "video":
+                if (page.src) markdown = `![](${page.src})`;
+                break;
         }
         if (markdown) {
-            markdown = FRONTMATTER + `title: "${notename}"\n` + `aliases: "${notename}"\n` + `foundryId: journal-${journal._id}\n` + FRONTMATTER + (markdown || "");
-            zip.file(`${dirname}/${validFilename(notename)}.md`, markdown, {binary:false});
+            markdown = FRONTMATTER + `title: "${page.name}"\n` + `aliases: "${journal.name}"\n` + `foundryId: ${page.uuid}\n` + FRONTMATTER + (markdown || "");
+            zip.file(`${dirname}/${validFilename(notename)}.md`, markdown, { binary: false });
         }
     }
 }
@@ -212,7 +199,7 @@ export function exportMarkdown(from, zipname) {
         return;
     }
 
-    zip.generateAsync({type:"blob"}).then((blob) => saveDataToFile(blob, `${validFilename(zipname)}.zip`));
+    zip.generateAsync({ type: "blob" }).then((blob) => saveDataToFile(blob, `${validFilename(zipname)}.zip`));
 }
 
 Hooks.once('init', async () => {
@@ -220,7 +207,7 @@ Hooks.once('init', async () => {
 
     // FOLDER context menu
     function addFolderMenu(wrapped, ...args) {
-       return wrapped(...args).concat({
+        return wrapped(...args).concat({
             name: `${MODULE_NAME}.exportToMarkdown`,
             icon: '<i class="fas fa-file-zip"></i>',
             callback: async header => {
@@ -236,30 +223,30 @@ Hooks.once('init', async () => {
     // JOURNAL ENTRY context menu
     function addEntryMenu(wrapped, ...args) {
         return wrapped(...args).concat({
-             name: `${MODULE_NAME}.exportToMarkdown`,
-             icon: '<i class="fas fa-file-zip"></i>',
-             callback: async header => {
+            name: `${MODULE_NAME}.exportToMarkdown`,
+            icon: '<i class="fas fa-file-zip"></i>',
+            callback: async header => {
                 const li = header.closest(".directory-item");
                 const entry = this.collection.get(li.data("entryId"));
-                if ( !entry ) return;
+                if (!entry) return;
                 exportMarkdown(entry, entry.name)
-             },
-         });
+            },
+        });
     }
-    libWrapper.register(MODULE_NAME, "JournalDirectory.prototype._getEntryContextOptions",  addEntryMenu,  libWrapper.WRAPPER);
+    libWrapper.register(MODULE_NAME, "JournalDirectory.prototype._getEntryContextOptions", addEntryMenu, libWrapper.WRAPPER);
 
     // JOURNAL Directory button  
 })
 
 Hooks.on("renderSidebarTab", async (app, html) => {
     if (game.user.isGM && app instanceof JournalDirectory) {
-      let button = $("<button class='import-cd'><i class='fas fa-file-import'></i>Export to Markdown</button>")
-      button.click(function () {
-        exportMarkdown(app, "JournalDirectory");
-      });
-      
-	  // 3.5 SRD doesn't have a directory-header
-	  let anchor = html.find(".directory-footer");
-      anchor.append(button);
+        let button = $("<button class='import-cd'><i class='fas fa-file-import'></i>Export to Markdown</button>")
+        button.click(function () {
+            exportMarkdown(app, "JournalDirectory");
+        });
+
+        // 3.5 SRD doesn't have a directory-header
+        let anchor = html.find(".directory-footer");
+        anchor.append(button);
     }
 })
