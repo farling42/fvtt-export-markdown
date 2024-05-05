9ace88d600c878a819ae97642eaf8f8b5901bff4
Ambient Silence <github@fractalsilence.com>
2024-05-05 14:54:05 -0400

    Make separate handlebar-helper file

--- scripts/handlebar-helpers.js
+++ scripts/handlebar-helpers.js
@@ -1,17 +1,36 @@
-//import * from "export-markdown.js";
+import { /*validFileName,*/ convertHtml, fileconvert } from "./export-markdown.js";
 
 export const registerHandlebarsHelpers = function () {
-
-        Handlebars.registerHelper('validFileName', function (name) {
+    
+    //Handlebar Helpers
+    
+    // Escapes file names to make them valid
+    Handlebars.registerHelper('validFileName', function (name) {
                 return validFilename(name);
             });
 
-        Handlebars.registerHelper('convertHtml', function (doc, html) {
-                return validFilename(name);
-            });
+    // Convert HTML to Markdown
+    Handlebars.registerHelper('convertHtml', function (context, text) {
+        if (text) {
+            text = convertHtml(context, text);
+        }
+        return text;
+        });
 
 
-        Handlebars.registerHelper('fileconvert', function (filename, label_or_size) {
-                return validFilename(name);
-            });
+    // Converts an image file reference to a wikilinks Markdown format
+    Handlebars.registerHelper('fileconvert', function (filename, label_or_size) {
+            return fileconvert(filename, label_or_size);
+        });
+
+    // Retrieves the items of a given type
+    Handlebars.registerHelper('itemsOfType', function (items, type) {
+        let _itemsOfType = [];
+        for (let item of items) {
+            if(item.type === type) {
+                _itemsOfType.push(item);
+            }
+        }
+        return _itemsOfType;
+    });
 }
